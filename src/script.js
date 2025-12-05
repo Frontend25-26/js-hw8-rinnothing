9d22b9839beaaea5bd94330bb8d23471b3520da2
rin-chan <58170199+rinnothing@users.noreply.github.com>
2025-12-05 20:00:51 +0300

    added animations of movement

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const board = document.getElementById("board");
+const fieldSize = 8;
 
 function createBoard() {
     for (let i = 0; i < 8; i++) {
@@ -31,4 +32,75 @@ function addPiece(cell, color, row, col) {
     cell.appendChild(piece);
 }
 
+function colorToDirection(piece) {
+    if (piece.classList.contains("white")) {
+        return -1;
+    }
+    return 1;
+}
+
+function isCorrectMove(iFrom, jFrom, iTo, jTo, direction) {
+    if (iTo < 0 || jTo < 0 || iTo >= fieldSize || jTo >= fieldSize) {
+        return false;
+    }
+    if (Math.abs(jFrom - jTo) != 1) {
+        return false;
+    }
+    return iTo - iFrom == direction;
+}
+
+function movePiece(piece, iTo, jTo) {
+    let cell = piece.closest(".cell");
+
+    let newRow = board.querySelectorAll(".row")[iTo];
+    let newCell = newRow.querySelectorAll(".cell")[jTo];
+    let dI = newCell.getBoundingClientRect().y - cell.getBoundingClientRect().y;
+    let dJ = newCell.getBoundingClientRect().x - cell.getBoundingClientRect().x;
+
+    piece.style.transform = `translate(${dJ}px, ${dI}px)`;
+
+    setTimeout(() => {
+        piece.style.transform = "translate(0px, 0px)";
+        cell.removeChild(piece);
+        newCell.appendChild(piece);
+    }, "300");
+}
+
+function addBoardHandlers() {
+    let pieceSelected = false;
+    let i, j;
+    let piece;
+    let direction;
+
+    board.addEventListener("click", function(evt) {
+        if (!pieceSelected) {
+            piece = evt.target.closest(".piece");
+            if (!piece) {
+                pieceSelected = false;
+                return;
+            }
+            direction = colorToDirection(piece);
+
+            let cell = piece.closest(".cell");
+            i = cell.dataset.i;
+            j = cell.dataset.j;
+            pieceSelected = true;
+        } else {
+            let cell = evt.target.closest(".cell");
+            if (!cell) {
+                pieceSelected = false;
+                return;
+            }
+
+            let iTo = cell.dataset.i;
+            let jTo = cell.dataset.j;
+            if (isCorrectMove(i, j, iTo, jTo, direction)) {
+                movePiece(piece, iTo, jTo);
+            }
+            pieceSelected = false;
+        }
+    });
+}
+
 createBoard();
+addBoardHandlers();
